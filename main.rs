a71e3f092eab1471dd3406eef8eb948d08b1e854
Stepan Butorin <Kuramchika@gmail.com>
2023-04-22 10:05:15 +0800

    lab2b

--- main.rs
+++ main.rs
@@ -2,7 +2,7 @@ use std::io::Write;
 use std::process::Command;
 
 fn main() {
-    let output = Command::new("iris-quilifier-method")
+    let output = Command::new("irisqualifier-method")
         .output()
         .expect("Failed to execute command");
 
