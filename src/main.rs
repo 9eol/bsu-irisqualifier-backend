8225fc6a26470f16ed8900f24bd4fd9863911703
Stepan Butorin <Kuramchika@gmail.com>
2023-04-22 09:10:22 +0800

    updated main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,3 @@
-use std::io::Write;
-use std::process::Command;
-
 fn main() {
-    let output = Command::new("python")
-        .arg("/home/s/projects/bsu/bragin-course/iris-qualifier/method")
-        .output()
-        .expect("Failed to execute command");
-
-    let mut out = std::io::stdout();
-    out.write_all(output.stdout.as_slice())
-        .expect("Failed to expecute command");
+    println!("Hello, world!");
 }
